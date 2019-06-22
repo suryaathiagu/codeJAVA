20fcf6e828665cf935d464ffa8550eac06f56f34
suryaa thiagarajan <47815224+suryaathiagu@users.noreply.github.com>
2019-06-22 15:57:42 +0530

    User ID Generation
    Step1: Compare the lengths of First_Name and Last_Name of the participant. The one that is shorter will be called "Smaller Name" and the one that is longer will be called the "Larger Name". If  both First_Name and Last_Name are of equal length then the name that appears earlier in alphabetical order will be called "Smaller Name" and the name that appears later in alphabetical order will be called the "Longer Name"
     Step2: The user-id should be generated as below-
     Last Letter of the smaller name+Entire word of the longer name+Digit at position N in the PIN when traversing the PIN from left to right+Digit at position N in the PIN when traversing the PIN from right to left
     Step3:Toggle the alphabets to the user-id generated in step2  i.e. upper-case alphabets should become lower-case and lower-case alphabets become upper-case.
     Let us see a few examples.
     Example-1 – If the participant's details are as below-
     First_Name = Rajiv
     Last_ Name =Roy
     PIN =560037
     N =6
     Step1-  Length of Last_Name is less than the Length of First_Name, so the Smaller Name is "Roy" and the Longer Name is "Rajiv"
     Step2- The user-id will be = Last Letter of the smaller name+Entire word in the longer name+Digit at position N in the PIN when traversing the PIN from left to right+Digit at position N in the PIN when traversing the PIN from right to left
     = Last Letter of "Roy"+Entire word in "Rajiv"+6th Digit of PIN from left+6th Digit of PIN from right
     =y+Rajiv+7+5
     Therefore, user-id = yRajiv75
     Step3- Toggle the alphabets in the uder-id. So, user-id =YrAJIV75

---
+++ userid.java
@@ -0,0 +1,67 @@
+import java.util.*;
+class userid
+{
+    public static void main(String... args)
+    {
+        Scanner s=new Scanner(System.in);
+        String str=s.next();
+        String str1=s.next();
+        int d=s.nextInt();
+        int n=s.nextInt();
+        int len=str.length();
+        int len1=str1.length();
+        List<String> al=new ArrayList<>();
+        String sarr[]=new String[2];
+        String s1,s11="";
+        if(len==len1)
+        {
+            sarr[0]=str;
+            sarr[1]=str1;
+            Arrays.sort(sarr);
+            String strr=sarr[0];
+            String strr1=sarr[1];
+            int le=strr.length();
+            s1=strr.substring(le-1,le);
+            s11=strr1;
+            //break;
+        }
+        else if(len>len1)
+        {
+             s1=str1.substring(len1-1,len1);
+             s11=str;
+        }
+        else
+        {
+            s1=str.substring(len-1,len);
+            s11=str1;
+        }
+        String s2=String.valueOf(d);
+        int len3=s2.length();
+        char s3=s2.charAt(n-1);
+        char s4=s2.charAt(len3-n);
+        String st;
+        String res=s1+""+s11+""+s3+""+s4;
+        for(int i=0;i<res.length();i++)
+        {
+            char c=res.charAt(i);
+            if(c>='A' && c<='Z')
+            {
+                st=Character.toString(c);
+                al.add(st.toLowerCase());
+            }
+            else
+            {
+                st=Character.toString(c);
+                al.add(st.toUpperCase());
+            }
+        }
+        for(int i=0;i<al.size();i++)
+        {
+            System.out.print(al.get(i));
+        }
+    }
+}
+
+
+
+
