ee8ac729a2139240bf83866c88514cba5937b9e2
suryaa thiagarajan <47815224+suryaathiagu@users.noreply.github.com>
2019-06-22 15:55:49 +0530

    sum of non-prime index values in an array
    A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself. In other words, a prime number is a whole number greater than 1, whose only two whole-number factor are 1 and itself. The first few prime number are 2,3,5,7,11,13,17,19,23 and 29.
    Given an array with ‘N’ elements, you are expected to find the sum of the values that are present in non-prime indexes of the array. Note that the array index starts with 0 i.e, the position (index) of the first array element is 0, the position of the next array element is 1, and so on.

---
+++ nonprime.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class nonprime
+{
+    public static void main(String... args)
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        List<Integer> al=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        //int k=0;
+        al.add(0);
+        al.add(1);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=2;j<i;j++)
+            {
+                if(i%j==0)
+                {
+                    al.add(i);
+                    break;
+                }
+            }
+        }
+        int sum=0;
+        for(int i=0;i<al.size();i++)
+        {
+            int m=al.get(i);
+            sum+=a[m];
+        }
+        System.out.print(sum);
+    }
+}
