1afeca3a46fa5ba38c149c16f8207f98ed4a61a9
suryaa thiagarajan <47815224+suryaathiagu@users.noreply.github.com>
2019-06-28 07:25:33 +0530

    Update armstrong.java

--- armstrong.java
+++ armstrong.java
@@ -7,12 +7,14 @@ class armstrong
     {
         Scanner s=new Scanner(System.in);
         int m=s.nextInt();
+        String str=String.valueOf(m);
+        int len=str.length();
         int n,rem,res=0;
         n=m;
         while(n>0)
         {
             rem=n%10;
-            res+=Math.pow(rem,3);
+            res+=Math.pow(rem,len);
             n=n/10;
         }
         if(res==m)
